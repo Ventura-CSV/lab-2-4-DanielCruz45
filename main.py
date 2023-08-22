9a8d1140fcdf1c48cae5a5979534bc7d45399e64
DanielCruz45 <dcruz44@ucmerced.edu>
2023-08-22 14:06:12 -0700

    norm order run fail, new test as slides showed

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
     original_str = "Python Programming"
     sub1 = original_str[:7]
     sub2 = original_str[7:18]
-    merge_str = sub1 + sub2
+    merge_str = sub2 + sub1
     
     print(sub2)
     print(sub1)
