29241f4d85265f8d2e236325ac34160f6e1ffdca
DanielCruz45 <dcruz44@ucmerced.edu>
2023-08-22 14:04:30 -0700

    space with words correct order

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ def main():
     # Comlete your code here
     ##################################################
     original_str = "Python Programming"
-    sub1 = original_str[:6]
+    sub1 = original_str[:7]
     sub2 = original_str[7:18]
-    merge_str = sub2 + sub1
+    merge_str = sub1 + sub2
     
     print(sub2)
     print(sub1)
