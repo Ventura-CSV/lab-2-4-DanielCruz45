e2adeea182a760fe3a5a8bc55e105e32d2e9d949
DanielCruz45 <dcruz44@ucmerced.edu>
2023-08-22 13:36:26 -0700

    missing a space

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
+    original_str = "Python Programming"
+    sub1 = original_str[0:6]
+    sub2 = original_str[7:18]
+    merge_str = sub2 + sub1
+    
+    print(sub2)
+    print(sub1)
+    print(merge_str)
+    
     pass
 
 
